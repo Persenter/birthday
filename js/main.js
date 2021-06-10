d2bad63412b857bd3ad0bad0e2c41fa91dcfb547
Persenter <84247074+Persenter@users.noreply.github.com>
2021-06-11 00:09:18 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let keys = ['1511','2252']
-function Check(codes, number) {
-	keys[number] = keys[number].replace('5','');
-	if(codes == keys[number])
-	_CPUThresholder(keys[number]);
-}
-function _CPUThresholder(manifest){
-window.location.href="https://persenter.github.io/birthday/"+manifest+".html";
-}
